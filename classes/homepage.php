67f7a28a38ddbf3da62817b792268e14a39e603b
Dale <yl622@njit.edu>
2017-11-01 17:32:31 -0400

    Added a sentence to let other know what is the SQL query in homepage.php

--- classes/homepage.php
+++ classes/homepage.php
@@ -5,6 +5,7 @@
             $getRes = accounts::homeworkSearch();
             if (empty($errorMas)) {
                 $this->html .= htmlTags::changeRow(htmlTags::headingOne("Connected Successfully"));
+                $this->html .= htmlTags::changeRow('The SQL query is "SELECT * FROM accounts WHERE id < 6"');
                 $this->html .= htmlTags::changeRow('There are ' . arrayFunctions::countNumber($getRes, 0) . ' records in the database');
                 $this->html .= table::createTable($getRes);
             } else {
